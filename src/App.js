4fb76e4db7259788a0a93ba9c698ef26368045af
keitheous <keithchongwy@gmail.com>
2018-05-09 17:29:59 +1000

    rendering response to page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ import Weather from "./components/Weather"
 const API_KEY = "4020ec17d93e3c219af0db2fcf860cac"
 
 class App extends React.Component {
+
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
   // older version of react requires you to set up the method and
   // constructor seperately to bind it to App.
   // this.getWeather = this.getWeather.bind(this);
@@ -18,10 +27,45 @@ class App extends React.Component {
   // weather api browser url -  http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22
   // async await - great way to make api call
   getWeather = async (e) => {
+
     e.preventDefault()
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=kuching,malaysia&appid=${API_KEY}&units=metric`);
-    const data = await api_call.json();
-    console.log(data)
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+    );
+    const data = await api_call.json(); // response from api
+    // console.log(data)
+    // set state here - dont directly manipulate the data using this.state.temperature and etc.. use setState built in method
+    // this.setState({
+    //   temperature: data.main.temp,
+    //   city: data.name,
+    //   country: data.sys.country,
+    //   humidity: data.main.humidity,
+    //   description: data.weather[0].description,
+    //   error: ''
+    // })
+    if (city && country){
+      console.log(data)
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ''
+      })
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: 'Please enter the values.'
+      })
+    }
+
   }
 
   render(){
@@ -30,7 +74,14 @@ class App extends React.Component {
 
         <Titles/>
         <Form getWeather={this.getWeather}/>
-        <Weather />
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
 
       </div>
     )
